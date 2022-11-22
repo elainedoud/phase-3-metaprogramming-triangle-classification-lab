6b556450dbfd3ec795275f912105fc107f7e14aa
Elaine <elainedoud@gmail.com>
2022-11-22 15:03:58 -0500

    commit tree 11cfb12b5d2f36d75fd42188edf13da44e03861f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+
+  attr_accessor :aSide, :bSide, :cSide
+  
+  def initialize(aSide,bSide,cSide)
+    @aSide = aSide
+    @bSide = bSide
+    @cSide = cSide
+  end
+
+  def kind
+    validate_triange
+    if (aSide = bSide = cSide)
+      :equilateral
+    elsif ((aSide = bSide) || (bSide = cSide) || (aSide = cSide))
+      :isosceles
+    else
+      :scalene
+    end
+  end 
+
+  def sides_greater_than_zero?
+    [aSide,bSide,cSide].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    aSide + bSide > cSide && aSide + cSide > bSide && bSide + cSide > aSide 
+  end
+
+  def validate_triangle
+    raise TriangeError unless sides_greater_than_zero && valid_triangle_inequality?
+  end
+
+  class TriangeError < StandardError
+  end
+
 end
